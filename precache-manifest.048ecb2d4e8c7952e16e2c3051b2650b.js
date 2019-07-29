6165a35a952cea074dc18acd84da2004cdec6bb3
gloryfrs <gloryfrs@yandex.ru>
2019-07-29 17:12:29 +0700

    Updates

---
+++ precache-manifest.048ecb2d4e8c7952e16e2c3051b2650b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "af01b5037ff63cf05210745f4c248269",
+    "url": "/lc-mim/static/media/RobotoBold.af01b503.woff"
+  },
+  {
+    "revision": "0b7d143d6be29139c145",
+    "url": "/lc-mim/static/css/main.9c176bed.chunk.css"
+  },
+  {
+    "revision": "b2c24342409e47baaeb690d76cbd7df3",
+    "url": "/lc-mim/static/media/RobotoBold.b2c24342.ttf"
+  },
+  {
+    "revision": "c99f4e9616af4283ccae",
+    "url": "/lc-mim/static/js/1.c99f4e96.chunk.js"
+  },
+  {
+    "revision": "0446f39cefc539ba302d",
+    "url": "/lc-mim/static/js/runtime~main.0446f39c.js"
+  },
+  {
+    "revision": "56e512fda303971690591fba90370521",
+    "url": "/lc-mim/static/media/loading.56e512fd.gif"
+  },
+  {
+    "revision": "b0112773bce4c26d7c68c7597cb0f403",
+    "url": "/lc-mim/static/media/country-pointer-geo-location-japan-512.b0112773.png"
+  },
+  {
+    "revision": "c19990cf18e5c5254498303f16a98246",
+    "url": "/lc-mim/static/media/banner.c19990cf.png"
+  },
+  {
+    "revision": "0b7d143d6be29139c145",
+    "url": "/lc-mim/static/js/main.0b7d143d.chunk.js"
+  },
+  {
+    "revision": "cbb753be961007a7e782f57f7eacbc94",
+    "url": "/lc-mim/static/media/RobotoBold.cbb753be.eot"
+  },
+  {
+    "revision": "eee713d6454874d4d1d8babd8dd562cf",
+    "url": "/lc-mim/static/media/RobotoRegular.eee713d6.eot"
+  },
+  {
+    "revision": "290793a328775e85f880f7da86503cd2",
+    "url": "/lc-mim/static/media/RobotoRegular.290793a3.ttf"
+  },
+  {
+    "revision": "3100f91bbd9e9ca9ecd00255cac7d11c",
+    "url": "/lc-mim/static/media/GothamPro.3100f91b.ttf"
+  },
+  {
+    "revision": "18b2429ba6e7179daeec5438639ab65f",
+    "url": "/lc-mim/static/media/RobotoRegular.18b2429b.woff"
+  },
+  {
+    "revision": "c99f4e9616af4283ccae",
+    "url": "/lc-mim/static/css/1.fb6bc5b4.chunk.css"
+  },
+  {
+    "revision": "3aa7eb481cea9c105e9cbf3436435fdf",
+    "url": "/lc-mim/index.html"
+  }
+];
